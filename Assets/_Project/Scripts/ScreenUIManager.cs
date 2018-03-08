fcdc658a0a68f02c0f5fd1f3af511b454325a140
y3lousso <y3lousso@gmail.com>
2018-03-07 20:28:03 -0500

    Finish audio + Add UI for scene + Rework scene design + Change controller apparence

---
+++ Assets/_Project/Scripts/ScreenUIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenUIManager : MonoBehaviour {
+
+    public GameObject AppTab;
+    public GameObject SoundTab;
+
+	// Use this for initialization
+	void Start () {
+        CloseTabs();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnAppButtonPressed()
+    {
+        CloseTabs();
+        AppTab.SetActive(true);
+    }
+
+    public void OnSoundButtonPressed()
+    {
+        CloseTabs();
+        SoundTab.SetActive(true);
+    }
+
+    public void OnExitButtonPressed()
+    {
+        SceneManager.instance.ChangeScene("Menu");
+    }
+
+    public void CloseTabs()
+    {
+        AppTab.SetActive(false);
+        SoundTab.SetActive(false);
+    }
+}
