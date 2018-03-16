030ceac31e9d1096ae737f6b27053dcf9bb7f36c
y3lousso <y3lousso@gmail.com>
2018-03-16 18:20:37 -0400

    Reset repo + Remove Nested Prefab + Remove Imports

---
+++ Assets/Scripts/FlickeringObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringObject : MonoBehaviour {
+
+    public bool IsFlickering { get; set; }
+
+    public Color flickeringColor;
+    public AnimationCurve flickeringCurve;
+    private MeshRenderer meshRenderer;
+
+    private float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+        IsFlickering = false;
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.EnableKeyword("_EMISSION");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsFlickering)
+        {
+            time += Time.deltaTime;
+            Color color = Color.Lerp(Color.black, flickeringColor, flickeringCurve.Evaluate(time));
+            meshRenderer.material.SetColor("_EmissionColor", color);
+        }
+        else
+        {
+            meshRenderer.material.SetColor("_EmissionColor", Color.black);
+        }
+	}
+}
