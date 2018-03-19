3eea977182f1737c7517495fadcf57dc567f7ca7
Winnilis <agathe.guillemot@gmail.com>
2018-03-19 13:06:39 -0400

    step 18

--- Assets/Scripts/FirstAids/Steps/FirstAid_Step18.cs
+++ Assets/Scripts/FirstAids/Steps/FirstAid_Step18.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class FirstAid_Step18 : BasicStep
 {
+	[Header("Specific Step18")]
+	[SerializeField] private AudioSource source;
+	[SerializeField] private AudioClip phone;
 
-    // Start after the activation of the step
-    public override void Enter()
-    {
+    public override void Enter(){}
 
-    }
+    public override void Exit(){}
 
-    // Start before the completion of the step
-    public override void Exit()
-    {
+	public void StartPhone(){
+		StartCoroutine ("PhoneActivation");
+	}
 
-    }
+	IEnumerator PhoneActivation(){
+		Debug.Log ("Phone activation");
+
+		source.clip = phone;
+		source.Play ();
+		yield return new WaitForSeconds (phone.length);
+
+		this.Complete ();
+	}
 }
