3175ed93f3c95b6575f94d2eb6f5f90407fd43b3
Hatim-Pierre Fazileabasse <hatim.pierre.fazileabasse@gmail.com>
2018-03-21 00:30:40 -0400

    Finished audio trigger utility, usable in a sperate thread to avoid freezing GUI, edited step 06 and 16 to add audio detection capabilities
    Signed-off-by: Hatim-Pierre Fazileabasse <hatim.pierre.fazileabasse@gmail.com>

--- Assets/Scripts/FirstAids/Steps/FirstAid_Step06.cs
+++ Assets/Scripts/FirstAids/Steps/FirstAid_Step06.cs
@@ -1,25 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class FirstAid_Step06 : BasicStep
 {
-
+    Thread audio_detect_th;
     void Update()
     {
         if ( (LiamInteraction.instance.leftHand.isTouching | LiamInteraction.instance.rightHand.isTouching ) && AudioTrigger.instance.isTalking)
         {
+            audio_detect_th.Abort();
             this.Complete();
         }
     }
     // Start after the activation of the step
     public override void Enter()
     {
-        // Hopefully this won't block everything...
-        do
-        {
-            AudioTrigger.instance.DetectAudio();
-        } while (!AudioTrigger.instance.isTalking);
+        audio_detect_th = new Thread(new ThreadStart(AudioTrigger.instance.DetectAudio));
+        audio_detect_th.Start();
     }
 
     // Start before the completion of the step
--- Assets/Scripts/FirstAids/Steps/FirstAid_Step16.cs
+++ Assets/Scripts/FirstAids/Steps/FirstAid_Step16.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class FirstAid_Step16 : BasicStep
 {
-
+    Thread audio_detect_th;
+    void Update()
+    {
+        if (AudioTrigger.instance.isTalking)
+        {
+            audio_detect_th.Abort();
+            this.Complete();
+        }
+    }
     // Start after the activation of the step
     public override void Enter()
     {
-
+        audio_detect_th = new Thread(new ThreadStart(AudioTrigger.instance.DetectAudio));
+        audio_detect_th.Start();
     }
 
     // Start before the completion of the step
